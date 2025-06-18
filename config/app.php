6cc94080ca2ac6220ab11981dc32e7e4eeb05f06
nurfandi31 <fandinur241@gmail.com>
2025-06-18 16:08:57 +0700

    app

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Snappy\ServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
     ])->toArray(),
 
     /*
