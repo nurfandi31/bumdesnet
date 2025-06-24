bdb7cb208b0d252fd353d45a2e3870280bf5d2f8
nurfandi31 <fandinur241@gmail.com>
2025-06-24 11:41:58 +0700

    instalasi

--- app/Http/Controllers/InstallationsController.php
+++ app/Http/Controllers/InstallationsController.php
@@ -494,7 +494,7 @@ class InstallationsController extends Controller
                     'tgl_transaksi' => Tanggal::tglNasional($request->order),
                     'total' => $jumlah_instal,
                     'installation_id' => $install->id,
-                    'keterangan' => 'Biaya instalasi An. ' . $install->customer->nama . ' (' . $install->kode_instalasi . ')',
+                    'keterangan' => 'Biaya instalasi atas nama ' . $install->customer->nama . ' (' . $install->kode_instalasi . ')',
                 ]);
             } else {
                 return response()->json([
