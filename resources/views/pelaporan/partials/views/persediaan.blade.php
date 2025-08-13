013dd3ceff05519b47c33d4d3993e94c6adbf85c
arafi118 <arp46301@gmail.com>
2025-08-13 09:50:54 +0700

    Perbaikan tampilan

--- resources/views/pelaporan/partials/views/persediaan.blade.php
+++ resources/views/pelaporan/partials/views/persediaan.blade.php
@@ -20,19 +20,21 @@
 <table border="0" width="100%" cellspacing="0" cellpadding="0" style="font-size: 12px;">
     <thead>
         <tr style="background: rgb(235, 234, 234);">
-            <th>Nama Barang</th>
-            <th>Stok</th>
-            <th>Satuan</th>
-            <th>Harga</th>
+            <th align="center" width="5%">No</th>
+            <th align="center" width="55%">Nama Barang</th>
+            <th align="center" width="10%">Stok</th>
+            <th align="center" width="10%">Satuan</th>
+            <th align="center" width="20%">Harga</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($products as $product)
             <tr class="{{ $loop->iteration % 2 == 1 ? 'row-white' : 'row-black' }}">
+                <td>{{ $loop->iteration }}</td>
                 <td>{{ $product->name }}</td>
-                <td>{{ $product->stok }}</td>
-                <td>{{ $product->unit->name }}</td>
-                <td>{{ number_format($product->harga_jual) }}</td>
+                <td align="center">{{ $product->stok }}</td>
+                <td align="center">{{ $product->unit->name }}</td>
+                <td align="right">{{ number_format($product->harga_jual) }}</td>
             </tr>
         @endforeach
     </tbody>
